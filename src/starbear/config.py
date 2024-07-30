8d08bb237d6c3b49069695a98db6460269b0c111
Olivier Breuleux <breuleux@gmail.com>
2024-07-29 22:30:14 -0400

    Add configuration through gifnoc

---
+++ src/starbear/config.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+import gifnoc
+
+
+@dataclass
+class StarbearDevConfig:
+    debug_mode: bool = False
+    inject: list[Any] = field(default_factory=list)
+
+
+@dataclass
+class StarbearConfig(gifnoc.Extensible):
+    dev: StarbearDevConfig = field(default_factory=StarbearDevConfig)
+
+
+config = gifnoc.define(
+    field="starbear",
+    model=StarbearConfig,
+)
