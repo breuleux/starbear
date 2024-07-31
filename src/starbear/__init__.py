283008f8e363a27289689495624ee9765810db53
Olivier Breuleux <breuleux@gmail.com>
2024-07-31 16:56:59 -0400

    Rename core.serve to core.app

--- src/starbear/__init__.py
+++ src/starbear/__init__.py
@@ -1,10 +1,10 @@
 from hrepr import H, J, returns
 
 from .config import config
+from .core.app import bear, simplebear
 from .core.constructors import BrowserEvent, FormData, NamespaceDict
 from .core.page import Component, Page, selector_for
 from .core.reg import Reference
-from .core.serve import bear, simplebear
 from .core.templating import Template, template
 from .core.utils import ClientWrap, FeedbackQueue, Queue, VirtualFile, rewrap
 from .version import version
