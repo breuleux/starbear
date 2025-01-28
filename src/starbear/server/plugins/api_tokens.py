a412f89925ba04ede330d774f23117424a269039
Olivier Breuleux <breuleux@gmail.com>
2025-01-28 15:00:00 -0500

    Fix api tokens bug, change how they are stored

--- src/starbear/server/plugins/api_tokens.py
+++ src/starbear/server/plugins/api_tokens.py
@@ -4,10 +4,11 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
-from apischema import deserialize, serialize
+from apischema import ValidationError, deserialize, serialize
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import PlainTextResponse
 
+from ...common import logger
 from ..config import StarbearServerPlugin
 from .permissions import make_config
 
@@ -25,7 +26,6 @@ class APITokensMiddleware(BaseHTTPMiddleware):
 
     async def dispatch(self, request, call_next):
         key = request.headers.get("X-API-KEY", None)
-        print("Key?", key)
         if not key:
             return await call_next(request)
         user = self.mapper(key)
@@ -38,6 +38,8 @@ class APITokensMiddleware(BaseHTTPMiddleware):
 
 @dataclass
 class TokenData:
+    name: str
+    token: str
     email: str
     # expiry: Optional[datetime.datetime] = None
     # plain: bool = True
@@ -49,7 +51,7 @@ class APITokens(StarbearServerPlugin):
     file: Optional[Path] = None
 
     # Default tokens
-    defaults: Optional[dict[str, TokenData]] = None
+    defaults: Optional[list[TokenData]] = None
 
     def cap_require(self):
         return ["session"]
@@ -65,17 +67,23 @@ class APITokens(StarbearServerPlugin):
         return self.config.read()
 
     def write(self, new_tokens, dry=False):
+        deserialize(list[TokenData], self.config.parse(new_tokens))
         if self.config.write(new_tokens, dry=dry) and not dry:
             self.reset()
 
     def reset(self):
         self.config.reset()
-        self.data = deserialize(dict[str, TokenData], self.config.dict)
+        try:
+            data = deserialize(list[TokenData], self.config.dict)
+        except ValidationError as exc:
+            logger.error("Could not read API tokens", exc_info=exc)
+            data = []
+        self.data = {token.token: token for token in data}
 
     def setup(self, server):
         assert self.file
         try:
-            self.config = make_config(self.file, defaults=serialize(self.defaults or {}))
+            self.config = make_config(self.file, defaults=serialize(self.defaults or []))
         except json.JSONDecodeError as exc:
             sys.exit(
                 f"ERROR decoding JSON: {exc}\n"
